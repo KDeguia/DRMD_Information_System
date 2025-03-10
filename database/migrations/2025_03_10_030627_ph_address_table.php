c83100388e1f3ca232db11714e83d35ee0e2031d
Kristian De Guia <kristiandizondeguia@gmail.com>
2025-03-10 11:09:19 +0800

    update name of table location

--- database/migrations/2025_03_10_030627_ph_address_table.php
+++ database/migrations/2025_03_10_030627_ph_address_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('locations', function (Blueprint $table) {
+        Schema::create('ph_address', function (Blueprint $table) {
             $table->id();
             $table->string('region');
             $table->string('province');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('locations');
+        Schema::dropIfExists('ph_address');
     }
 };
